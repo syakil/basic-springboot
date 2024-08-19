adb3c4451a0ddf3e433a74d63d5c70b373aab639
syakil <syakil@amanahcorp.co.id>
2024-08-19 17:59:03 +0700

    test

--- belajar/src/main/java/com/belajar/services/CategoryService.java
+++ belajar/src/main/java/com/belajar/services/CategoryService.java
@@ -1,4 +1,31 @@
 package com.belajar.services;
 
+import com.belajar.models.entities.Category;
+import com.belajar.models.repositories.CategoryRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
 public class CategoryService {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    public Category save(Category category) {
+        return categoryRepository.save(category);
+    }
+
+    public Iterable<Category> findAll() {
+        return categoryRepository.findAll();
+    }
+
+    public Category findById(Long id) {
+        return categoryRepository.findById(id).get();
+    }
+
+    public void deleteById(Long id) {
+        categoryRepository.deleteById(id);
+    }
 }
